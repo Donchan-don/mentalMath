2867f841e24371f5fab91db9706edaccea298c92
Donchan-Don <obaloluwaadejuwon101@gmail.com>
2025-03-13 12:57:09 +0100

    Mental math

---
+++ landingPage/landingPage.js
@@ -0,0 +1,8 @@
+const ADDITION_LINK = document.getElementById("additionLink")
+const SUBTRACTION_LINK = document.getElementById("subtractionLink")
+const MULTIPLICATION_LINK = document.getElementById("multiplicationLink")
+const DIVISION_LINK = document.getElementById("divisionLink")
+
+ADDITION_LINK.onclick = function(){
+    window.location.href = "Addition/addition.html"
+}
